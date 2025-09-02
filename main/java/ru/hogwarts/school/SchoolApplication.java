833599d42500f77be8bfd0a3b8ed8b3abb4f70b8
Admin <elimdar.ref@mail.ru>
2025-09-02 20:53:29 +0300

    ДЗ

--- main/java/ru/hogwarts/school/SchoolApplication.java
+++ main/java/ru/hogwarts/school/SchoolApplication.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 @OpenAPIDefinition
 public class SchoolApplication {
@@ -12,5 +11,4 @@ public class SchoolApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
 	}
-
 }
