19e8a3af975ce3774358ddd3033a5d720a86ba12
Admin <elimdar.ref@mail.ru>
2025-09-02 16:58:39 +0300

    LP

---
+++ main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,42 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class FacultyService {
+
+    @Autowired
+    private final FacultyRepository facultyRepository;
+
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty createFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Faculty findFaculty(Long id) {
+        return facultyRepository.findById(id).get();
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteFaculty(Long id) {
+        facultyRepository.deleteById(id);
+    }
+
+    public Collection<Faculty> findByColor(String color) {
+        return facultyRepository.findAll().stream()
+                .filter(faculty -> Objects.equals(faculty.getColor(), color))
+                .collect(Collectors.toList());
+    }
+}
